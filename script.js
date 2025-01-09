86b150b8e158de019abb2cb6105d6af904659b4d
Neeraj <ns892082@gmail.com>
2025-01-09 10:52:53 +0530

    up to date

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let chatContainer = document.querySelector(".chat-container");
 let userMessage = null;
 //Enter your gemini API key
 //Enter your gemini API key
-//Enter your gemini API key
+//Enter your gemini API keyg
 let Api_Url = '/*enter here*/';
 function createChatBox(html, className) {
     let div = document.createElement("div");
